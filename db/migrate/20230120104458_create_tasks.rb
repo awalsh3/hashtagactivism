0e816f2d5784b1808d6b4153a2f1fb052623d773
Annie Walsh <annie@anyonething.co.uk>
2023-01-20 10:46:22 +0000

    adds task model

---
+++ db/migrate/20230120104458_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.text :content
+      t.integer :time
+      t.string :url
+      t.references :cause, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_20_103330) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_20_104458) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,4 +20,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_20_103330) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.integer "time"
+    t.string "url"
+    t.bigint "cause_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cause_id"], name: "index_tasks_on_cause_id"
+  end
+
+  add_foreign_key "tasks", "causes"
 end
