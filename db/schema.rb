3414a3b88a540a0be5772bfef196015faa2fae19
Annie Walsh <annie@anyonething.co.uk>
2023-01-20 10:48:31 +0000

    Adds user model placeholder until devise needed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_20_104458) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_20_104757) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_20_104458) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["cause_id"], name: "index_tasks_on_cause_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "tasks", "causes"
