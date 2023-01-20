9945d650865d2a5e7b89abe67ba0666a0ca101ea
Annie Walsh <annie@anyonething.co.uk>
2023-01-20 10:51:18 +0000

    Adds action model

---
+++ db/migrate/20230120105024_create_actions.rb
@@ -0,0 +1,11 @@
+class CreateActions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :actions do |t|
+      t.boolean :completed
+      t.references :user, null: false, foreign_key: true
+      t.references :task, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_20_104757) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_20_105024) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "actions", force: :cascade do |t|
+    t.boolean "completed"
+    t.bigint "user_id", null: false
+    t.bigint "task_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_actions_on_task_id"
+    t.index ["user_id"], name: "index_actions_on_user_id"
+  end
 
   create_table "causes", force: :cascade do |t|
     t.string "name"
@@ -38,5 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_20_104757) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "actions", "tasks"
+  add_foreign_key "actions", "users"
   add_foreign_key "tasks", "causes"
 end
