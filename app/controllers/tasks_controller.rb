eb03bba41ef1e31c6f6032ea3ff468df19426570
Annie Walsh <annie@anyonething.co.uk>
2023-02-03 13:23:51 +0000

    config tasks controller views and seed data

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,8 @@
+class TasksController < ApplicationController
+
+  def index
+    @cause = Cause.find(params[:cause_id])
+    @tasks = Task.all
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -27,4 +27,61 @@ cause3 = Cause.create!(
 )
 cause3.photo.attach(io: File.open('app/assets/images/14.png'), filename: 'blm.png', content_type: 'png')
 
+task1 = Task.create!(
+  name: 'Sign this petition',
+  content: 'Make a difference by signing the petition on Change.org',
+  cause_id: cause1.id,
+  time: 10,
+  url: 'https://www.change.org/t/black-lives-matter-en-us'
+)
+
+task2 = Task.create!(
+  name: 'Email the representative at blm.com',
+  content: 'This service will assist you by matching your post code to your local branch, with links to your members website and contact page',
+  cause_id: cause1.id,
+  time: 20,
+  url: 'https://ag.ny.gov/contact-attorney-general-letitia-james'
+)
+
+  task3 = Task.create!(
+  name: 'Research and contact the BLAM movement',
+  content: 'Research the BLAM movement, a community outreach programme based around education, and find out what they can provide to your local area',
+  cause_id: cause1.id,
+  time: 30,
+  url: 'https://blamuk.org/'
+)
+
+task4 = Task.create!(
+  name: 'Anti racism resources',
+  content: 'A wide range of anti racism resources to research and get involved with',
+  cause_id: cause1.id,
+  time: 30,
+  url: 'https://survivorsnetwork.org.uk/anti-racism-resources/'
+)
+
+task5 = Task.create!(
+  name: 'Red Cross guide to teaching children about racism',
+  content: 'Raise awareness with children and young people about racism with this guide',
+  cause_id: cause1.id,
+  time: 30,
+  url: 'https://www.redcross.org.uk/get-involved/teaching-resources/talking-with-children-and-young-people-about-race-and-racism'
+)
+
+task6 = Task.create!(
+  name: 'Donate to legal aid',
+  content: 'Make a difference by donating to legal aid for BLM related cases',
+  cause_id: cause1.id,
+  time: 10,
+  url: 'https://www.change.org/t/black-lives-matter-en-us'
+)
+
+task7 = Task.create!(
+  name: 'Education petition',
+  content: 'Sign this petition to push for equality education',
+  cause_id: cause1.id,
+  time: 10,
+  url: 'https://www.change.org/t/black-lives-matter-en-us'
+)
+
+
 puts 'Seed: Finished seeding!'
