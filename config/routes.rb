4b8d033e8098feec79de090bb4f57ce5d7df2abd
Annie Walsh <annie@anyonething.co.uk>
2023-03-10 13:11:21 +0000

    cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,15 +9,13 @@ Rails.application.routes.draw do
       end
     end
 
-  root "pages#home"
-  get "about", to: "pages#about"
-  get "how_it_works", to: "pages#how_it_works"
-
-  get '/causes/:id/choose_cause', to: 'causes#choose_cause',as: 'choose_cause'
-  # get '/causes/:id/choose_time', to: 'causes#choose_time',as: 'choose_time'
-
   resources :causes, only: [ :index, :show ] do
       resources :tasks, only:  [ :index, :create ] do
         end
       end
+
+  root "pages#home"
+  get "about", to: "pages#about"
+  get "how_it_works", to: "pages#how_it_works"
+  get '/causes/:id/choose_cause', to: 'causes#choose_cause',as: 'choose_cause'
 end
