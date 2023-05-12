9d5309aa55a2ca501ab34b8495b3de8d31e4402f
Annie Walsh <annie@anyonething.co.uk>
2023-05-12 13:44:11 +0100

    Filters tasks by time

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,18 @@
 class TasksController < ApplicationController
+#   def index
+#     @cause = Cause.find(params[:cause_id])
+#     @tasks = Task.all
+#     if params[:max_time]
+#       @tasks = @tasks.where('time <= ?', params[:max_time])
+#     end
+#   end
+# end
 
   def index
     @cause = Cause.find(params[:cause_id])
-    @tasks = Task.all
+    @tasks = @cause.tasks
+    if params[:max_time]
+      @tasks = @tasks.where('time <= ?', params[:max_time])
+    end
   end
-
 end
